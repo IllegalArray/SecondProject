eeb167f858a0e2b92fdd444b8dbdecf8623e733f
pytho <pytho@192.168.0.4>
2020-07-14 22:54:57 -0500

    This is a second project

---
+++ src/oneplustwoequalstwelve/OnePlusTwoEqualsTwelve.java
@@ -0,0 +1,11 @@
+
+package oneplustwoequalstwelve;
+
+public class OnePlusTwoEqualsTwelve {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+Weather ot
